cc42b5b823e0ffdc60968c0eee26394a03ef015c
AbulBashar38 <abulbasarofficial5403@gmail.com>
2025-06-02 00:10:58 +0600

    add travel meds pdf

--- convert.js
+++ convert.js
@@ -2,12 +2,12 @@ const fs = require("fs");
 const pdf = require("html-pdf-node");
 
 const generatePDF = () => {
-  const html = fs.readFileSync("gml.html", "utf8");
+  const html = fs.readFileSync("twm.html", "utf8");
   const file = { content: html };
   const option = { format: "A4", orientation: "portrait" };
   pdf.generatePdf(file, option).then((pdfBuffer) => {
-    fs.writeFileSync("gml-convert.pdf", pdfBuffer);
-    console.log("PDF generated as gml.pdf");
+    fs.writeFileSync("twm-convert.pdf", pdfBuffer);
+    console.log("PDF generated as twm.pdf");
   });
 };
 
