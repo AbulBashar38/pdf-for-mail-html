676070b0a7983e93bf9f01f9787dcf7f9587e155
Tariqul Islam Rezvee <mdrezvee@gmail.com>
2025-06-06 03:05:10 +0600

    Refactor code structure for improved readability and maintainability

--- convert.js
+++ convert.js
@@ -3,8 +3,13 @@ const pdf = require("html-pdf-node");
 
 const generatePDF = () => {
   const html = fs.readFileSync("twm.html", "utf8");
-  const file = { content: html };
-  const option = { format: "A4", orientation: "portrait" };
+  // const file = { content: html };
+  const file = { url: "http://127.0.0.1:5501/gml.html" };
+  const option = {
+    format: "A4",
+    orientation: "portrait",
+    printBackground: true,
+  };
   pdf.generatePdf(file, option).then((pdfBuffer) => {
     fs.writeFileSync("twm-convert.pdf", pdfBuffer);
     console.log("PDF generated as twm.pdf");
